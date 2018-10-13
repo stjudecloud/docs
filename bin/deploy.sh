8ad8e649ff15c9c951a13eb033cb72520e3aabe5
Clay McLeod <clay.l.mcleod@gmail.com>
2018-10-12 20:58:57 -0500

    Added commit hash to the footer.
      - Altered theme to include commit hash in footer.
      - Changed ordering of mkdocs.yml to put the extra section at the
    bottom.
      - Modify deploy script to write commit hash to config file on build.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -16,6 +16,10 @@ chmod 600 ~/SJCloudDeploy
 eval `ssh-agent -s`
 ssh-add ~/SJCloudDeploy
 
+# Write the commit hash to the config file.
+COMMIT_HASH=$(git log --pretty=format:'%h' -n 1)
+echo "  commit_hash: \"$COMMIT_HASH\"" >> mkdocs.yml
+
 # Compile the HTML.
 if ! mkdocs build; then
     echo "Building the HTML failed! This requires investigation."
