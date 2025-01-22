02bb1d47aac3a3a5db491389a1d6eb539fa823a3
Kevin Benton <kbenton@stjude.org>
2025-01-22 11:24:34 -0600

    feat: :fire: removes footer links

--- app/app.config.ts
+++ app/app.config.ts
@@ -25,27 +25,7 @@ export default defineAppConfig({
   footer: {
     credits: 'St. Jude Children\'s Research Hospital © 2025',
     colorMode: false,
-    links: [{
-      'icon': 'i-simple-icons-nuxtdotjs',
-      'to': 'https://nuxt.com',
-      'target': '_blank',
-      'aria-label': 'Nuxt Website'
-    }, {
-      'icon': 'i-simple-icons-discord',
-      'to': 'https://discord.com/invite/ps2h6QT',
-      'target': '_blank',
-      'aria-label': 'Nuxt UI on Discord'
-    }, {
-      'icon': 'i-simple-icons-x',
-      'to': 'https://x.com/nuxt_js',
-      'target': '_blank',
-      'aria-label': 'Nuxt on X'
-    }, {
-      'icon': 'i-simple-icons-github',
-      'to': 'https://github.com/nuxt/ui',
-      'target': '_blank',
-      'aria-label': 'Nuxt UI on GitHub'
-    }]
+    links: []
   },
   toc: {
     title: 'Table of Contents',
