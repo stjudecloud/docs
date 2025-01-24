a0c3099a59be8d0a7144f3b42f4c7e4451076f67
Kevin Benton <kbenton@stjude.org>
2025-01-24 10:27:24 -0600

    style: :lipstick: updates header logo spacing to h-8

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -10,7 +10,7 @@ const { header } = useAppConfig()
   <UHeader>
     <template #logo>
       <template v-if="header?.logo?.dark || header?.logo?.light">
-        <UColorModeImage v-bind="{ class: 'h-6 w-auto', ...header?.logo }" /><UBadge
+        <UColorModeImage v-bind="{ class: 'h-8 w-auto', ...header?.logo }" /><UBadge
           label="Docs"
           variant="subtle"
           class="mb-0.5"
